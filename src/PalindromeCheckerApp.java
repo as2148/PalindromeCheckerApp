6fdb3ffbda94dcacc54a59f76d7a62603c9d39b9
as2148 <as2148@srmist.edu.in>
2026-02-25 16:16:36 +0530

    UC10

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,14 +1,9 @@
 public class PalindromeCheckerApp {
-
-    static boolean check(String text, int start, int end) {
-        if (start >= end) return true;
-        if (text.charAt(start) != text.charAt(end)) return false;
-        return check(text, start + 1, end - 1);
-    }
-
     public static void main(String[] args) {
-        String text = "madam";
-        boolean result = check(text, 0, text.length() - 1);
+        String text = "A man a plan a canal Panama";
+        String normalized = text.replaceAll("\\s+", "").toLowerCase();
+        String reversed = new StringBuilder(normalized).reverse().toString();
+        boolean result = normalized.equals(reversed);
         System.out.println("Input : " + text);
         System.out.println("Is Palindrome? : " + result);
     }
