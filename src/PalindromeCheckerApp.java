0a9d5ab89086753113d3edf5d55ef16b2bbd16bd
as2148 <as2148@srmist.edu.in>
2026-02-25 15:55:47 +0530

    UC8

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,19 +1,45 @@
-import java.util.*;
-
 public class PalindromeCheckerApp {
+
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) { this.data = data; }
+    }
+
     public static void main(String[] args) {
-        String text = "refer";
-        Deque<Character> deque = new LinkedList<>();
+        String text = "level";
+        Node head = null, tail = null;
         for (char c : text.toCharArray()) {
-            deque.add(c);
+            Node node = new Node(c);
+            if (head == null) head = tail = node;
+            else { tail.next = node; tail = node; }
         }
+
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null;
+        while (slow != null) {
+            Node next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        Node first = head, second = prev;
         boolean result = true;
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
+        while (second != null) {
+            if (first.data != second.data) {
                 result = false;
                 break;
             }
+            first = first.next;
+            second = second.next;
         }
+
         System.out.println("Input : " + text);
         System.out.println("Is Palindrome? : " + result);
     }
