28509120669111e4d0028fbab68f2f25fee1aee1
as2148 <as2148@srmist.edu.in>
2026-02-25 15:37:54 +0530

    UC5

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,18 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String text = "radar";
-        char[] arr = text.toCharArray();
+        String text = "noon";
+        Stack<Character> stack = new Stack<>();
+        for (char c : text.toCharArray()) {
+            stack.push(c);
+        }
         boolean result = true;
-        int start = 0;
-        int end = arr.length - 1;
-        while (start < end) {
-            if (arr[start] != arr[end]) {
+        for (char c : text.toCharArray()) {
+            if (c != stack.pop()) {
                 result = false;
                 break;
             }
-            start++;
-            end--;
         }
         System.out.println("Input : " + text);
         System.out.println("Is Palindrome? : " + result);
