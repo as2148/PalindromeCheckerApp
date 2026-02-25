14457244cb12f5b45c7f84eca781b47341a0a14a
as2148 <as2148@srmist.edu.in>
2026-02-25 15:49:54 +0530

    UC7

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,16 +2,14 @@ import java.util.*;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String text = "civic";
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
+        String text = "refer";
+        Deque<Character> deque = new LinkedList<>();
         for (char c : text.toCharArray()) {
-            queue.add(c);
-            stack.push(c);
+            deque.add(c);
         }
         boolean result = true;
-        while (!queue.isEmpty()) {
-            if (!queue.remove().equals(stack.pop())) {
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
                 result = false;
                 break;
             }
